2a33b257440aeb45dc32a1d4e74e3107651a3175
Mingyu Choi <mg010626@naver.com>
2023-10-13 18:29:41 +0900

    Feat | 캐릭터 정지 및 좌우이동 애니메이션 구현

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+
+    public float moveX = 1;
+
+    private bool isWalkFirst = true;
+    private bool isIdleNow = false;
+    private Animator animator;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * moveX;
+
+        moveDelta = new Vector3(x, 0, 0);
+
+        if (isWalkFirst && x != 0)
+        {
+            isWalkFirst = false;
+            isIdleNow = false;
+            animator.Play("walk");
+        }
+
+        if (isIdleNow && x == 0)
+        {
+            isWalkFirst = true;
+            isIdleNow = true;
+            animator.Play("idle");
+        }
+
+
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = new Vector3(2.5f, 2.5f, 1);
+        }    
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-2.5f, 2.5f, 1);
+        }
+        transform.Translate(moveDelta * Time.deltaTime * 2);
+    }
+}
