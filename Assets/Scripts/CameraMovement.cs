2566d979760f3e4b9e7a7584834442478b157d26
ssohee <prinso@naver.com>
2023-10-21 15:22:46 +0900

    falling death

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CameraMovement : MonoBehaviour
 {
     public float cameraSpeed = 5.0f;
-
+    public bool death = false;
     public GameObject player;
 
     void Update()
     {
+       
         Vector3 dir = player.transform.position - this.transform.position;
-        Vector3 moveVector = new Vector3(0.0f, (dir.y + 3.8f) * cameraSpeed * Time.deltaTime, 0.0f);
-        this.transform.Translate(moveVector);
+        if (dir.y + Camera.main.orthographicSize <0)
+        {
+            death = true;
+            Invoke("RestartGame", 0.1f);
+            
+        }
+        if (!death)
+        {
+            Vector3 moveVector = new Vector3(0.0f, (dir.y + 3.8f) * cameraSpeed * Time.deltaTime, 0.0f);
+            
+            this.transform.Translate(moveVector);
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene("GameOverScene");
     }
 }
