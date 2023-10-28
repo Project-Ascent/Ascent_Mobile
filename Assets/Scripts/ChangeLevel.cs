b2be6048d6e3f63bff1d09fab1e3b9a0daec86dc
Mingyu0626 <mg010626@naver.com>
2023-10-28 21:38:45 +0900

    Docs | 씬 이름 변경

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+
+    public int level;
+
+    public void SceneChange()
+    {
+        if (level == 1)
+        {
+            SceneManager.LoadScene("ClimbingLevel1");
+        }
+
+        if (level == 2)
+        {
+            SceneManager.LoadScene("ClimbingLevel2");
+        }
+        if (level == 3)
+        {
+            SceneManager.LoadScene("ClimbingLevel3");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
