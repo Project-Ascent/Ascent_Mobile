01b8276587c6565abbe7a9769db0dbc2dc811444
ssohee <prinso@naver.com>
2023-10-29 18:08:28 +0900

    bgm

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -23,7 +23,7 @@ public class Item : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        beans = true;
+        
     }
 
     // Update is called once per frame
