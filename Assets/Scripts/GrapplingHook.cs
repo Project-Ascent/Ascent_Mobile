808bb7053aaca578325925c029a0cac7e38d9dc2
Mingyu Choi <mg010626@naver.com>
2023-11-26 18:37:18 +0900

    Feat | 모바일 조작법 구현

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
@@ -13,14 +14,18 @@ public class GrapplingHook : MonoBehaviour
     public bool isHookFired;
     public bool isRangeMax;
     public bool isAttach;
+
+    public float hookableAreaWidthPercentage = 0.8f;
+    public float hookableAreaHeightPercentage = 0.8f;
+    private float hookableAreaWidth;
+    private float hookableAreaHeight;
+
+    private bool isMoveButtonPressed; // 수정된 변수
+
     void Start()
     {
-        line.positionCount = 2;
-        line.endWidth = line.startWidth = 0.05f;
-        line.SetPosition(0, transform.position);
-        line.SetPosition(1, hook.position);
-        line.useWorldSpace = true;
-        hook.gameObject.SetActive(false);
+        InitializeLineRenderer();
+        InitializeHook();
     }
 
     void Update()
@@ -28,45 +33,114 @@ public class GrapplingHook : MonoBehaviour
         line.SetPosition(0, transform.position);
         line.SetPosition(1, hook.position);
 
-        if (Input.GetMouseButtonDown(0) && !isHookFired)
+        // 이동 버튼을 최초로 눌렀을 때
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == UnityEngine.TouchPhase.Began)
         {
-            hook.position = transform.position;
-            mouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            isHookFired = true;
-            isRangeMax = false;
-            isAttach = false;
-            hook.gameObject.SetActive(true);
+            if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                Debug.Log("isMoveButtonPressed가 True로 바뀜");
+                isMoveButtonPressed = true;
+            }
+
+            // 훅이 발사되지 않은 상태에서만 훅을 발사
+            if (!isHookFired && isMoveButtonPressed)
+            {
+                Debug.Log("훅 발사");
+                FireHook();
+            }
         }
+
+        // 이동 버튼을 떼었을 때
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == UnityEngine.TouchPhase.Ended)
+        {
+            isMoveButtonPressed = false;
+        }
+
+
+        // 훅 발사 후의 동작들
         if (isHookFired && !isRangeMax)
         {
-            hook.Translate(mouseDir.normalized * Time.deltaTime * launchSpeed);
-
-            if (Vector2.Distance(transform.position, hook.position) > 10)
-            {
-                isRangeMax = true;
-            }
+            MoveHook();
+            CheckMaxRange();
         }
         else if (isHookFired && isRangeMax && !isAttach)
         {
-            hook.position = Vector2.MoveTowards(hook.position, transform.position, Time.deltaTime * 1000);
-            if (Vector2.Distance(transform.position, hook.position) < 0.1f)
-            {
-                isHookFired = false;
-                isRangeMax = false;
-                isAttach = false;
-                hook.gameObject.SetActive(false);
-            }
+            RetractHook();
         }
+
         else if (isAttach)
         {
-            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+            // 훅이 이미 붙어있는 상태에서 이동 버튼을 누를 때는 훅을 회수하지 않음
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == UnityEngine.TouchPhase.Began)
             {
-                isHookFired = false;
-                isRangeMax = false;
-                isAttach = false;
-                hook.GetComponent<Attached>().joint2D.enabled = false;
-                hook.gameObject.SetActive(false);
+                if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                {
+                    Debug.Log("현재 붙어있는 상태, 이동 버튼을 눌러도 훅이 회수되면 안돼");
+                    ResetHookState();
+                }
             }
         }
+    }
+
+    void InitializeLineRenderer()
+    {
+        line.positionCount = 2;
+        line.endWidth = line.startWidth = 0.05f;
+        line.useWorldSpace = true;
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, hook.position);
+    }
+
+    void InitializeHook()
+    {
+        hook.gameObject.SetActive(false);
+    }
+
+    void FireHook()
+    {
+        hook.position = transform.position;
+        mouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        isHookFired = true;
+        isRangeMax = false;
+        isAttach = false;
+        hook.gameObject.SetActive(true);
+    }
+
+    void MoveHook()
+    {
+        hook.Translate(mouseDir.normalized * Time.deltaTime * launchSpeed);
+
+        if (Vector2.Distance(transform.position, hook.position) > 10) // 훅의 최대 사거리
+        {
+            isRangeMax = true;
+        }
+    }
+
+    void CheckMaxRange()
+    {
+        if (Vector2.Distance(transform.position, hook.position) > 10) // 훅의 최대 사거리
+        {
+            isRangeMax = true;
+        }
+    }
+
+    void RetractHook()
+    {
+        hook.position = Vector2.MoveTowards(hook.position, transform.position, Time.deltaTime * 1000);
+
+        if (Vector2.Distance(transform.position, hook.position) < 0.1f)
+        {
+            ResetHookState();
+            isMoveButtonPressed = false; // 버튼을 떼면 이 변수를 false로 설정
+        }
+    }
+
+    void ResetHookState()
+    {
+        isHookFired = false;
+        isRangeMax = false;
+        isAttach = false;
+        hook.GetComponent<Attached>().joint2D.enabled = false;
+        hook.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -17,11 +17,18 @@ public class PlayerMove : MonoBehaviour
 
     GrapplingHook grappling;
 
+
+    public bool inputLeft = false;
+    public bool inputRight = false;
+
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
         grappling = GetComponent<GrapplingHook>();
+
+        UIButtonManager ui = GameObject.FindGameObjectWithTag("Managers").GetComponent<UIButtonManager>();
+        ui.init();
     }
 
     private void FixedUpdate()
@@ -29,7 +36,7 @@ public class PlayerMove : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal") * moveX;
 
         moveDelta = new Vector3(x, 0, 0);
-
+        // Debug.Log(moveDelta);
         if (isWalkFirst && x != 0 && !grappling.isAttach)
         {
             isWalkFirst = false;
@@ -52,11 +59,10 @@ public class PlayerMove : MonoBehaviour
         }
 
 
-
         if (moveDelta.x > 0)
         {
             transform.localScale = new Vector3(2.5f, 2.5f, 1);
-        }    
+        }
         else if (moveDelta.x < 0)
         {
             transform.localScale = new Vector3(-2.5f, 2.5f, 1);
@@ -66,10 +72,23 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
-        curY = transform.position.y;
-        if (curY > 100)
+        if (!inputLeft && !inputRight)
         {
-            SceneManager.LoadScene("BossStageScene");
+            animator.Play("idle");
+        }
+        else if (inputLeft)
+        {
+            animator.Play("move");
+            transform.localScale = new Vector3(-2.5f, 2.5f, 1);
+            moveDelta = new Vector3(moveX, 0, 0);
+            transform.Translate(-moveDelta * Time.deltaTime * 2);
+        }
+        else if (inputRight)
+        {
+            animator.Play("move");
+            transform.localScale = new Vector3(2.5f, 2.5f, 1);
+            moveDelta = new Vector3(moveX, 0, 0);
+            transform.Translate(moveDelta * Time.deltaTime * 2);
         }
     }
 }
