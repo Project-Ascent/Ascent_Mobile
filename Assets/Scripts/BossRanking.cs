e77771a0f2737039ca21f2475dbba0d00b61719b
ssohee <prinso@naver.com>
2023-11-27 20:40:12 +0900

    Finished Ranking sys

---
+++ Assets/Scripts/BossRanking.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRanking : MonoBehaviour
+{
+    float climbingTime;
+    float startTime;
+    float finishTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        climbingTime = PlayerPrefs.GetFloat("current");
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boss_life life = GameObject.Find("boss").GetComponent<boss_life>();
+        if (life.amount == 0)
+        {
+            finishTime = Time.time;
+            PlayerPrefs.SetFloat("current", finishTime - startTime);
+            print(PlayerPrefs.GetFloat("current"));
+        }
+    }
+}
