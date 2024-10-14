721adffc9a756d8165c1d125fcabb274e66c112f
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2024-10-14 15:34:18 +0900

    Add | 싱글톤 게임 매니저 추가

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public bool isTutorial = false;
+
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void LoadSceneWithName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
