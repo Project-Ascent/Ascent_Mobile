03ea817381d33815a71e03e90642d9269c09dce0
Mingyu Choi <mg010626@naver.com>
2024-10-19 21:18:30 +0900

    Refactor | Thorn(가시) 플랫폼 리팩토링

---
+++ Assets/Scripts/Platformer/Thorn/Thorn.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thorn : MonoBehaviour
+{
+    private int damage = 1;
+    private float invincibleTime = 3f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !HPManager.Instance.GetIsInvincible())
+        {
+            ApplyDamage();
+            StartCoroutine(ApplyInvincibility(invincibleTime));
+        }
+    }
+
+    void ApplyDamage()
+    {
+        HPManager.Instance.DecreaseHP(damage);
+    }
+
+    private IEnumerator ApplyInvincibility(float invincibleTime)
+    {
+        HPManager.Instance.SetIsInvincible(true);
+        yield return new WaitForSeconds(invincibleTime);
+        HPManager.Instance.SetIsInvincible(false);
+    }
+}
